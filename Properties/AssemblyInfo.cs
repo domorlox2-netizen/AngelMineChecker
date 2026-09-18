18f7e8020c363396a4db1019a52e224c5aec6f7f
domorlox2-netizen <domorlox2@gmail.com>
2026-09-18 21:43:24 +0300

    Add AngelMine Checker source code

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("AngelMine Checker")]
+[assembly: AssemblyDescription("Специализированный чекер для проверок на сервере AngelMine")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AngelMine Network")]
+[assembly: AssemblyProduct("AngelMine Checker")]
+[assembly: AssemblyCopyright("Copyright © AngelMine 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,
+    ResourceDictionaryLocation.SourceAssembly
+)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
